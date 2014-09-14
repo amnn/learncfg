b75d47ad130c3a3e04376c33f7104b6f2c21ede8
Ashok Menon <amenon94@gmail.com>
2014-09-14 09:50:59 +0100

    CFG: Adding method and test stubs.

---
+++ cfg/src/cfg/cfg.clj
@@ -0,0 +1,11 @@
+(ns cfg.cfg)
+
+(defmacro cfg [rules])
+
+(defmacro rule [body])
+
+(defn add-rule [g r])
+
+(defn rule-seq [g])
+
+(defn non-term-seq [g])
---
+++ cfg/test/cfg/cfg_test.clj
@@ -0,0 +1,13 @@
+(ns cfg.cfg-test
+  (:require [clojure.test :refer :all]
+            [cfg.cfg :refer :all]))
+
+(deftest cfg-test)
+
+(deftest rule-test)
+
+(deftest add-rule-test)
+
+(deftest rule-seq-test)
+
+(deftest non-term-seq-test)
