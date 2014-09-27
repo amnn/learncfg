820443cf92c36012a8c187bf1eafdf55030888d9
Ashok Menon <amenon94@gmail.com>
2014-09-27 22:02:01 +0100

    Util: Adding `combine` function.
     * Re-adding `interleave*`, as it is necessary in `combine`.

--- cfg/src/cfg/util.clj
+++ cfg/src/cfg/util.clj
@@ -6,6 +6,26 @@
   (both potentially infinite), leaving no elements discarded."
   [xs ys]
   (lazy-seq
-    (if-let [[x & xs*] (seq xs)]
-      (cons x (interleave ys xs*))
+    (if-let [xs* (seq xs)]
+      (cons (first xs*) (interleave ys (rest xs*)))
       ys)))
+
+(defn interleave*
+  "Converts a sequence of potentially infinite sequences into its lazy
+  interleaving."
+  [xss]
+  (lazy-seq
+    (when-let [xss* (seq xss)]
+      (interleave (first xss*)
+                  (interleave* (rest xss*))))))
+
+(defn combine
+  "Takes a finite sequence of potentially infinite sequences, and combines
+  them to produce a possibly infinite sequence of their cartesian product."
+  [xss]
+  (if-let [xss* (seq xss)]
+    (interleave*
+      (for [x (first xss*)]
+        (for [cs (combine (rest xss*))]
+          (lazy-seq (cons x cs)))))
+    '(())))
