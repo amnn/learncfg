489757c5fc41a1c63e11b39bfec40ae7f9ee47af
Ashok Menon <amenon94@gmail.com>
2014-10-14 01:42:15 +0100

    Lang: Adding lang-seq tests.

--- cfg/test/cfg/lang_test.clj
+++ cfg/test/cfg/lang_test.clj
@@ -38,3 +38,24 @@
     (let [in-lang? (in-lang (cfg (:S => )))]
       (is (in-lang? []))
       (is (not (in-lang? '[A]))))))
+
+(deftest lang-seq-test
+  (testing "left recursion"
+    (let [g (cfg (:S => :S A | ))]
+      (is (= '([] [A] [A A] [A A A] [A A A A])
+             (take 5 (lang-seq g))))))
+
+  (testing "right recursion"
+    (let [g (cfg (:S => A :S | ))]
+      (is (= '([] [A] [A A] [A A A] [A A A A])
+             (take 5 (lang-seq g))))))
+
+  (testing "product rules"
+    (let [ws (take 4 (lang-seq (cfg (:S => A :S B :S | ))))]
+      (is (every? '#{[] [A B] [A B A B] [A A B B]} ws))
+      (is (= 4 (count (into #{} ws))))))
+
+  (testing "ambiguous grammar"
+    (let [ws (take 4 (lang-seq (cfg (:S => :S :S | A :S B | ))))]
+      (is (every? '#{[] [A B] [A B A B] [A A B B]} ws))
+      (is (= 4 (count (into #{} ws)))))))
