78a2327b165b7be4494e59a14d5e71d67da5e94c
Ashok Menon <amenon94@gmail.com>
2014-10-25 22:17:17 +0100

    Simple: Refactoring merge-nts in terms of new utility functions.

--- cfg/src/cfg/learn/simple.clj
+++ cfg/src/cfg/learn/simple.clj
@@ -1,5 +1,6 @@
 (ns cfg.learn.simple
-  (:require [clojure.set :refer [union]]))
+  (:require [clojure.set :refer [union]]
+            [cfg.cfg :refer [mapr remove-nt]]))
 
 (defn merge-nts
   "Combine the rules for `nt1` and `nt2` under one terminal (`nt1`), replacing
@@ -10,11 +11,6 @@
   (let [r1 (g nt1), r2 (g nt2)
         nt-map {nt2 nt1}
         new-nts #(get nt-map % %)]
-    (into
-      {} (map (fn [kvp]
-                (update-in kvp [1]
-                  #(into #{}
-                         (map (partial mapv new-nts) %))))
-              (-> g
-                  (assoc nt1 (union r1 r2))
-                  (dissoc nt2))))))
+    (mapr new-nts
+          (-> (remove-nt g nt2)
+              (assoc nt1 (union r1 r2))))))
