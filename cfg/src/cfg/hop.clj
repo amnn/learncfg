44fafd333aef46981f61849bac67c4ab329a60b5
Ashok Menon <amenon94@gmail.com>
2015-03-23 22:13:41 +0000

    Hop Counts: Getting rid of superfluous test (always true)

--- cfg/src/cfg/hop.clj
+++ cfg/src/cfg/hop.clj
@@ -25,22 +25,21 @@
                   (map no-hops)
                   (into (priority-map)))]
       (if-let [[nt hops] (peek q)]
-        (let [c-hops (get counts nt)]
-          (if (and c-hops (< c-hops hops))
-            (recur counts (pop q))
-            (let [new-counts (assoc! counts nt hops)]
-              (recur
-               new-counts
-               (into (pop q)
-                     (for [r (hop-graph nt)
-                           :let  [old-hops (get q (:nt @r))]
-                           :when (or (not old-hops) (> old-hops hops))
-                           :let  [{:keys [not-visited nt rule]}
-                                  (visit-rule r)]
-                           :when (zero? not-visited)]
-                       [nt (->> (filter non-terminal? rule)
-                                (map new-counts)
-                                (reduce +) inc)]))))))
+        (if (get counts nt)
+          (recur counts (pop q))
+          (let [new-counts (assoc! counts nt hops)]
+            (recur
+             new-counts
+             (into (pop q)
+                   (for [r (hop-graph nt)
+                         :let  [old-hops (get q (:nt @r))]
+                         :when (or (not old-hops) (> old-hops hops))
+                         :let  [{:keys [not-visited nt rule]}
+                                (visit-rule r)]
+                         :when (zero? not-visited)]
+                     [nt (->> (filter non-terminal? rule)
+                              (map new-counts)
+                              (reduce +) inc)])))))
         (persistent! counts)))))
 
 (defn best-rules
